e82135f3553057300d1c849e37ffc663c806d741
watakaberei <126738738+watakaberei@users.noreply.github.com>
2023-09-26 13:40:39 +0000

    関数の演習

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-let i;
-let num = 0;
+// メイン部分
+let alertString;
 
-for(i = 1; i < 11; i++){
-  num = num + i;
+// 作成した関数を呼び出し、変数へ格納
+alertString = addString("WebCamp");
+
+//変数の中身をアラートで表示する
+alert(alertString);
+
+// 作成した関数
+function addString(strA){
+  let addStr = "Hello " + strA;
+  return addStr;
 }
-
-alert('1から10まで足し算した結果は' + num + 'です');
